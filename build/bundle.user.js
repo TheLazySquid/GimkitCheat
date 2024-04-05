1410e21c37e2bee6bbec0ef6d44b4eff2a31aee8
helloIlovegimkit <164534666+helloIlovegimkit@users.noreply.github.com>
2024-04-05 11:42:36 -0500

    Update bundle.user.js

--- build/bundle.user.js
+++ build/bundle.user.js
@@ -20567,7 +20567,7 @@
 				let angle = getUnsafeWindow().Phaser.Math.Angle.Between(0, 0, vector.x, vector.y);
 				socketManager.sendMessage("FIRE", { angle, x: body.x, y: body.y });
 			},
-			50
+			0.001
 		);
 
 		function togglebutton_enabled_binding(value) {
